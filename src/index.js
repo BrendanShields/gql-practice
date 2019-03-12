245aad3cb14f65765c0878566151c3667cf3948f
Brendan Shields <brendan.m.j.shields@gmail.com>
2019-03-12 19:18:41 +1100

    basic structure for gql

---
+++ src/index.js
@@ -0,0 +1,92 @@
+import { GraphQLServer } from 'graphql-yoga'
+
+// type definitions (schema)
+
+const users = [{
+    id: '1',
+    name: 'Andrew',
+    email: 'andrew@example.com',
+    age: 27
+}, {
+    id: '2',
+    name: 'sarah',
+    email: 'sara@example.com'
+}, {
+    id: '3',
+    name: 'Mike',
+    email: 'mike@example.com'
+}]
+
+const posts = [{
+    id: '1',
+    title: 'help',
+    body: 'coding forever and ever and ever'
+}, {
+    id: '2',
+    title: 'help',
+    body: 'more eggs on the roof'
+}]
+
+const typeDefs = `
+    type Query {
+        users(query: String): [User!]!
+        me: User! 
+        posts(query: String): [Post!]!
+    }
+    
+    type User {
+        id: ID!
+        name: String!
+        email: String!
+        age: Int
+    }
+
+    type Post {
+        id: ID!
+        title: String!
+        body: String!
+    }
+`
+//resolver or api
+
+const resolvers = {
+    Query: {
+        users(parent, args, ctx, info) {
+            if (!args.query) {
+                return users
+            }
+
+            return users.filter((user) => {
+                return user.name.toLowerCase().includes(args.query.toLowerCase())
+            })
+        },
+        me() {
+            return {
+                id: '123098',
+                name: 'Brendan',
+                email: 'brendan.m.j.shields@gmail.com',
+                age: 29
+            }
+        },
+        posts(parent, args, ctx, info) {
+            if (!args.query) {
+                return posts
+            }
+
+            return posts.filter((post) => {
+                const titleMatch = post.title.toLowerCase().includes(args.query.toLowerCase());
+                const bodyMatch = post.body.toLowerCase().includes(args.query.toLowerCase());
+                return titleMatch || bodyMatch
+            })
+        }
+     }
+}
+
+const server = new GraphQLServer({
+    typeDefs,
+    resolvers
+})
+
+server.start(() => {
+    console.log('running')
+})
